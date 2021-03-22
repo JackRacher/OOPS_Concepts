a071b6cc0daaf4d6ed18288bbe788fd3a60f9a1c
pb21071 <pb21071@OJAS-DD402.corp.ojas-it.com>
2021-03-22 15:32:19 +0530

    Edited files

--- src/simple_OOPS_Program/Dice_Game.java
+++ src/simple_OOPS_Program/Dice_Game.java
@@ -2,30 +2,32 @@ package simple_OOPS_Program;
 
 import java.util.*;
 
-class Dice{
+class Dice {
 
 	int faceValue;
+
 	void roll() {
 		Random random = new Random();
 		faceValue = random.nextInt(6) + 1;
 	}
 }
 
-class Player{
+class Player {
 	String pname;
 	int pValue;
+
 	void accept(String name) {
 		pname = name;
 	}
-	
-	void throwDice(Dice d1, Dice  d2) {
+
+	void throwDice(Dice d1, Dice d2) {
 		d1.roll();
 		d2.roll();
-		
+
 		pValue = d1.faceValue + d2.faceValue;
-		System.out.println(pValue + " = " + d1.faceValue +" + " + d2.faceValue);
+		System.out.println(pValue + " = " + d1.faceValue + " + " + d2.faceValue);
 	}
-	
+
 }
 
 public class Dice_Game {
@@ -34,27 +36,25 @@ public class Dice_Game {
 		Scanner scanner = new Scanner(System.in);
 		Dice d1 = new Dice();
 		Dice d2 = new Dice();
-		
+
 		Player p1 = new Player();
 		Player p2 = new Player();
-		
+
 		System.out.println("Enter p1 name: ");
 		p1.accept(scanner.next());
-		
+
 		System.out.println("Enter p1 name: ");
 		p1.accept(scanner.next());
-		
+
 		p1.throwDice(d1, d2);
 		p2.throwDice(d1, d2);
-		
+
 		String re = "";
-		if(p1.pValue > p2.pValue) {
+		if (p1.pValue > p2.pValue) {
 			System.out.println(p1.pname + "p1 wins");
-		}
-		else if(p2.pValue > p1.pValue) {
+		} else if (p2.pValue > p1.pValue) {
 			System.out.println(p2.pname + "p1 wins");
-		}
-		else {
+		} else {
 			re = "Try Again";
 		}
 		System.out.println(re);
