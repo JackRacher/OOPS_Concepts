5cd264fc643d1e85aae2b275d1733c03bff79fc7
pb21071 <pb21071@ojas-dl440.corp.ojas-it.com>
2023-01-05 18:24:49 +0530

    done

--- src/sample/Demo.java
+++ src/sample/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Welcome");
+		String s = "Ram";
+		System.out.println(s.charAt(0));
 	}
 }
