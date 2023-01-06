51c44b40aff29433156ea65e6e0e2f73f338983f
pb21071 <pb21071@ojas-dl440.corp.ojas-it.com>
2023-01-06 11:21:42 +0530

    done

---
+++ src/sample/ReversOfString.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class ReversOfString {
+	public static void main(String[] args) {
+		String s1="Hello world";
+		String s2="";
+		for (int i = s1.length()-1; i >= 0; i--) {
+			s2 = s2+s1.charAt(i);
+		}
+		System.out.println(s2);
+	}
+
+}
--- src/sample/Strings_Examples.java
+++ src/sample/Strings_Examples.java
@@ -3,9 +3,9 @@ package sample;
 public class Strings_Examples {
 
 	public static void main(String[] args) {
-		String s1 = "array";
-		String s2 = ""+s1.charAt(0);
-		for (int i = 1; i < s1.length(); i++) {
+		String s1 = "arraypapaaaa";
+		String s2 = "";
+		for (int i = 0; i < s1.length(); i++) {
 			char s  = s1.charAt(i);
 			int count = 0;
 			for (int j = 0; j < s2.length(); j++) {
