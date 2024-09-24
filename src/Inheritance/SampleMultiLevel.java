abfd709fc9b6a59cfa3c4fd7c4d03fee73b276ea
JackRacher <pruthviraj.pr74@gmail.com>
2024-09-24 15:21:27 +0530

    Added

--- src/Inheritance/SampleMultiLevel.java
+++ src/Inheritance/SampleMultiLevel.java
@@ -1,5 +1,7 @@
 package Inheritance;
 
+import java.math.BigInteger;
+
 class A{
 	A(){
 		System.out.println("A class constructor");
@@ -16,9 +18,23 @@ class C extends B{
 		System.out.println("c class constructor");
 	}
 }
+
+class BaseClass{
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+}
+
+class DerivedClass extends BaseClass{
+	public static int sum(int a, int b) {
+		return 2*a + 2*b;
+	}
+}
+
 public class SampleMultiLevel {
 
 	public static void main(String[] args) {
 		C obj = new C();
+		
 	}
 }
